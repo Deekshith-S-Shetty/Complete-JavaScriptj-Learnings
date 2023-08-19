82b9c24dc6f73f6615ae686d0040b3e2c0192e83
Deekshith Shetty <deekshithshetty698@gmail.com>
2023-08-19 22:20:36 +0530

    Add theory and examples for arithmetic operators

---
+++ Operators.js
@@ -0,0 +1,29 @@
+let a = 5,
+  b = 22;
+
+// ARITHMETIC OPERATORS
+// + -> Addition
+console.log(a + b);
+
+// - -> Subtraction
+console.log(a - b);
+
+// * -> Multiplication
+console.log(a * b);
+
+// / -> Division
+console.log(a / b);
+
+// % -> Modulus
+console.log(a % b); // The remainder of 2 numbers is the modulo (Here its 5)
+
+// ** -> Exponentiation
+console.log(b ** a); // b to the power a
+
+// ++ -> Increment
+let c = a++; // Stores "a" value in "c" and then increases the value of "a" by 1 (c = 5, a = 6)
+let d = ++b; // First increments the value of "b" by 1 and then stores the value in "d" (b = 23, d = 23)
+
+// -- -> Decrement
+let e = a--; // Stores the value of "a" in "e" and the decrements the value by 1
+let f = --a; // First decrements the value of "a" by 1 and then stores its value in "f"
