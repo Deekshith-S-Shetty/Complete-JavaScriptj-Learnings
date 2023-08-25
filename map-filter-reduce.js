f1b45c53e51052d5b441cda56b6dcf1369033d00
Deekshith Shetty <deekshithshetty698@gmail.com>
2023-08-25 22:33:36 +0530

    Add theory and examples on filter function in js

--- map-filter-reduce.js
+++ map-filter-reduce.js
@@ -5,3 +5,10 @@ let arr2 = arr.map((value, index, array) => {
   return value * value;
 });
 console.log(arr2);
+
+// Filter -> Filters an array and creates a new array with the values that passes the condition.
+arr = [45, 56, 23, 54, 43, 77];
+arr2 = arr.filter((value) => {
+  return value > 50;
+});
+console.log("\n", arr2);
